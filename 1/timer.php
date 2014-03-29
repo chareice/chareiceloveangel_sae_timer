36a864c432305e56be4de1f51d620db60aceaabb
chareice@qq.com <chareice@qq.com@f5698e54-7b7c-4ada-9adf-7085dd5b2482>
2014-03-29 11:00:54 +0000

    set begin time
    git-svn-id: https://svn.sinaapp.com/chareiceloveangel@2 f5698e54-7b7c-4ada-9adf-7085dd5b2482

---
+++ 1/timer.php
@@ -0,0 +1,33 @@
+<?php
+date_default_timezone_set("Asia/Shanghai");
+class Together{
+	public static $begin;
+	public static function getTogetherTime(){
+		self::$begin = strtotime('2013-02-18 20:30');
+		$now = time();
+		$timeSecond = $now - self::$begin;
+		return array(
+			'begin' => self::$begin,
+			'now' => $now,
+			'timeSecond' => $timeSecond,
+			'days' => intval($timeSecond / (60*60*24)),
+			'hours'=> intval(($timeSecond / (60*60)) % 24),
+			'minutes' => intval(($timeSecond / 60) % 60),
+			'seconds' => intval($timeSecond % 60)
+			);
+	}
+}
+$request_header = array();
+foreach(getallheaders() as $name => $value){
+    $request_header[$name] = $value;
+}
+if(isset($request_header['Origin'])){
+    header("Access-Control-Allow-Origin: {$request_header['Origin']}");
+}
+if(isset($_GET['callback'])){
+	header("Content-type:application/x-javascript");
+	echo sprintf("%s(%s)",$_GET['callback'],json_encode(Together::getTogetherTime()));
+}else{
+	header("Content-type:application/json");
+	echo json_encode(Together::getTogetherTime());
+}
